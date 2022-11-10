3668f24af1f6bc6e42533bf886e515a72d525b43
Hasan Hüseyin Demir <hasan.01imam01@gmail.com>
2022-11-10 03:25:10 +0300

    Create Deneme.cs

---
+++ Deneme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversi
+{
+    class Reversi
+    {
+        static void Main(string[] arguman)
+        {
+            Console.WriteLine("Bu oyun Reversi (Merhaba).");
+            // While döngüsünü kapsülleme
+            OyunTahtası oyun = new OyunTahtası();
+            oyun.print();
+
+            Tuple<int, int> move;
+            int sıra, sutun;
+            char onayla = 'N';
